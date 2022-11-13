3c7460d143a93a750b820a250b30ddd932f7b955
toby king <97766459+tobiasking@users.noreply.github.com>
2022-11-13 16:44:53 -0500

    basic navbar completed

--- src/main/java/org/liftoff/project/Controllers/LogIn.java
+++ src/main/java/org/liftoff/project/Controllers/LogIn.java
@@ -1,7 +1,15 @@
 package org.liftoff.project.Controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class LogIn {
+
+    @RequestMapping(value="/login")
+    @ResponseBody
+    public String index() {
+        return "Login Page";
+    }
 }
