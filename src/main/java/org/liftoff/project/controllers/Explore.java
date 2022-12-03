24bfb243fa836bc5494cc5c99115044c928f907e
emily-holz <emily.r.holzman@gmail.com>
2022-12-03 11:15:57 -0500

    trying to connect Thymeleaf template template to DB

--- src/main/java/org/liftoff/project/controllers/Explore.java
+++ src/main/java/org/liftoff/project/controllers/Explore.java
@@ -1,13 +1,21 @@
 package org.liftoff.project.controllers;
 
+import org.liftoff.project.data.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class Explore {
 
-    @RequestMapping(value="/explore")
-    public String explore(){
+    @Autowired
+    private PostRepository postRepository;
+
+    @GetMapping(value="/explore")
+    public String displayMostRecentPosts(Model model){
+
+        model.addAttribute("posts", postRepository.findAll());
         return "explore";
     }
 }
