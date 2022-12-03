65c981acadf41cc344b03832fc2d8df6a3e9fbca
emily-holz <emily.r.holzman@gmail.com>
2022-12-03 11:50:42 -0500

    connected thymeleaf template to database

--- src/main/java/org/liftoff/project/controllers/Explore.java
+++ src/main/java/org/liftoff/project/controllers/Explore.java
@@ -1,20 +1,26 @@
 package org.liftoff.project.controllers;
 
+import org.liftoff.project.data.CategoryRepository;
 import org.liftoff.project.data.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/explore")
 public class Explore {
 
     @Autowired
     private PostRepository postRepository;
 
-    @GetMapping(value="/explore")
-    public String displayMostRecentPosts(Model model){
+    @Autowired
+    private CategoryRepository categoryRepository;
 
+    @GetMapping
+    public String displayMostRecentPosts(Model model){
+        model.addAttribute("categories", categoryRepository.findAll());
         model.addAttribute("posts", postRepository.findAll());
         return "explore";
     }
