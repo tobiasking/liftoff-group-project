1a53155de1903b00f5517e5c6d6216758f34abcf
Chris Reams <chrisreams91@gmail.com>
2022-12-07 21:12:59 -0600

    added a button

--- src/main/java/org/liftoff/project/controllers/Explore.java
+++ src/main/java/org/liftoff/project/controllers/Explore.java
@@ -1,15 +1,37 @@
 package org.liftoff.project.controllers;
 
+import org.liftoff.project.data.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequestMapping("/explore-page")
 public class Explore {
 
-    @RequestMapping(value="/explore")
-    @ResponseBody
-    public String index(){
-        return "Exploration Page";
+    @Autowired
+    private PostRepository posttRepository;
+
+    private int counter;
+
+    @GetMapping
+    public String displayOldestPosts(Model model){
+        counter = 0;
+        model.addAttribute("posts", posttRepository.findByOrderByIdAsc());
+        model.addAttribute("counter", counter);
+        return "explore";
     }
+
+    @PostMapping()
+    public String test(Model model) {
+        counter += 1;
+        model.addAttribute("counter", counter);
+        return "explore";
+    }
+
+
 }
