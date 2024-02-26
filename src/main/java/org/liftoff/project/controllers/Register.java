d8effee06eac862eddb3bc4dd4babd8abecf2dca
toby king <97766459+tobiasking@users.noreply.github.com>
2024-02-25 23:33:28 -0500

    Create Register.java

---
+++ src/main/java/org/liftoff/project/controllers/Register.java
@@ -0,0 +1,39 @@
+
+package org.liftoff.project.controllers;
+
+import javax.validation.Valid;
+import org.liftoff.project.data.UserRepository;
+import org.liftoff.project.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Register {
+    @Autowired
+    UserRepository userRepository;
+
+    @RequestMapping("/register")
+    public String getUserRegistration(Model model) {
+        User userRegister = new User();
+        model.addAttribute("userRegister", userRegister);
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String postUserRegistration(@Valid User userRegister, Errors errors) {
+        System.out.println("Entering postUserRegistration method");
+
+        if (errors.hasErrors()) {
+            System.out.println("Validation errors: " + errors.getAllErrors());
+            return "redirect:/register";
+        } else {
+            userRepository.save(userRegister);
+            return "redirect:/";
+        }
+    }
+
+        }
